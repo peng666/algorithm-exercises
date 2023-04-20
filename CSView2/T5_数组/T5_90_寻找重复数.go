40ca4377f9499e425111787b7d95947af414e860
peng666 <jieweipeng@qq.com>
2023-04-20 12:15:20 +0800

    1

--- CSView2/T5_数组/T5_90_寻找重复数.go
+++ CSView2/T5_数组/T5_90_寻找重复数.go
@@ -1,1 +1,20 @@
 package main
+
+/*
+这道题就是：想象成环形链表，找环的入口
+想象快慢指针走的次数相等（想想）
+*/
+
+func findDuplicate(nums []int) int {
+	slow, fast := nums[0], nums[nums[0]]
+	for slow != fast { // 找环的的部分，进去环里面
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+	}
+	slow = 0
+	for fast != slow { // 在环里面相遇，就是重复元素(环的入口）
+		fast = nums[fast]
+		slow = nums[slow]
+	}
+	return slow
+}
--- CSView2/T8_回溯/T8_01_全排列.go
+++ CSView2/T8_回溯/T8_01_全排列.go
@@ -1,1 +1,36 @@
-package T8_回溯
+package main
+
+/*
+
+ */
+func permute(nums []int) [][]int {
+	result := [][]int{}
+	tmp := []int{}
+	visited := make([]bool, len(nums))
+	var dfs func([]int)
+	dfs = func(nums []int) {
+		if len(nums) == len(tmp) {
+			result = append(result, append([]int{}, tmp...))
+			// 注意，切片实际上是一个地址（指针）result存的是各个指针
+			// 而tmp是外部变量，没有重新赋值，指向的是一直不变的
+			// 所以，每次都要创建一个新空间，深拷贝
+		}
+		for i := 0; i < len(nums); i++ {
+			if visited[i] {
+				continue
+			}
+			visited[i] = true
+			tmp = append(tmp, nums[i])
+			dfs(nums)
+			tmp = tmp[:len(tmp)-1]
+			visited[i] = false
+		}
+	}
+	dfs(nums)
+	return result
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	permute(nums)
+}
